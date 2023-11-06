7240b5cf63129d6ec933a9710786f89be877f90e
Kyle Rozanitis <kyle.rozanitis@gmail.com>
2023-11-06 17:24:45 +0100

    NYX-10: Update NYX token code and add deployment scripts

---
+++ contracts/NyxToken.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity ^0.8.9;
+
+import "@lukso/lsp-smart-contracts/contracts/LSP7DigitalAsset/presets/LSP7Mintable.sol";
+import "@lukso/lsp-smart-contracts/contracts/LSP7DigitalAsset/extensions/LSP7Burnable.sol";
+
+contract NyxToken is LSP7Mintable, LSP7Burnable {
+    constructor() LSP7Mintable("NYX Token", "NYX", msg.sender, false) {
+        mint(msg.sender, 20000 * 10 ** decimals(), true, "0x");
+    }
+}
