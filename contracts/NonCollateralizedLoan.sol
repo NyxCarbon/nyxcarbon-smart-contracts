93f23c5a44fac9f2c195eae3e53a5b3a76baeca9
Kyle Rozanitis <kyle.rozanitis@gmail.com>
2023-11-07 19:07:43 +0100

    NYX-10: Add tests for loan contract

--- contracts/NonCollateralizedLoan.sol
+++ contracts/NonCollateralizedLoan.sol
@@ -1,38 +1,55 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.4;
 
-import { LSP7Mintable } from "@lukso/lsp-smart-contracts/contracts/LSP7DigitalAsset/presets/LSP7Mintable.sol";
+import {LSP7Mintable} from "@lukso/lsp-smart-contracts/contracts/LSP7DigitalAsset/presets/LSP7Mintable.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
 contract NonCollateralizedLoan {
+    using SafeMath for uint256;
+
     LSP7Mintable public token;
 
-    struct LoanTerms {
-        uint256 amount;
-        uint256 apy;
-        uint256 amortizationPeriodInMonths;
-        uint256 lockUpPeriodInMonths;
-        uint256 transactionPercentage;
+    enum LoanState {
+        Created,
+        Funded,
+        Taken,
+        Repayed,
+        Liquidated
     }
-    LoanTerms public loanTerms;
-
-    enum LoanState {Created, Funded, Taken, Repayed, Liquidated}
     LoanState public loanState;
 
     address payable public lender;
     address payable public borrower;
     address payable public nyxCarbonAddress;
+
+    uint256 public amount;
+    uint256 public apy;
+    uint256 public amortizationPeriodInMonths;
+    uint256 public lockUpPeriodInMonths;
+    uint256 public transactionPercentage;
+
     address payable public tokenAddress;
-    
-    uint256 public remainingBalance;
+
     uint256 public transactionFee;
     uint256 public netMonthlyPayment;
     uint256 public dueDate;
 
-    constructor(LoanTerms memory _terms, address payable _tokenAddress, address payable _nyxCarbonAddress) {
-        loanTerms = _terms;
-        tokenAddress = _tokenAddress;
+    constructor(
+        uint256 _amount,
+        uint256 _apy,
+        uint256 _amortizationPeriodInMonths,
+        uint256 _lockUpPeriodInMonths,
+        uint256 _transactionPercentage,
+        address payable _tokenAddress,
+        address payable _nyxCarbonAddress
+    ) {
+        amount = _amount * 1e18;
+        apy = _apy * 1e18;
+        amortizationPeriodInMonths = _amortizationPeriodInMonths;
+        lockUpPeriodInMonths = _lockUpPeriodInMonths;
+        transactionPercentage = _transactionPercentage;
+        token = LSP7Mintable(_tokenAddress);
         nyxCarbonAddress = _nyxCarbonAddress;
-        remainingBalance = _terms.amount;
         lender = payable(msg.sender);
         loanState = LoanState.Created;
     }
@@ -42,38 +59,74 @@ contract NonCollateralizedLoan {
         _;
     }
 
-    function fundLoan() public onlyInState(LoanState.Created) {
+    // Is this really necessary?!
+    modifier checkAuthorizedAmount() {
+        require(
+            token.authorizedAmountFor(address(this), msg.sender) >= amount,
+            "Contract is now authorized to send this amount of tokens"
+        );
+        _;
+    }
+
+    function fundLoan()
+        public
+        payable
+        onlyInState(LoanState.Created)
+        checkAuthorizedAmount
+    {
+        token.transfer(msg.sender, address(this), amount, true, "0x");
         loanState = LoanState.Funded;
-        // Need to check if we can send to address without LSP1 Universal Receiver
-        LSP7Mintable(tokenAddress).transfer(msg.sender, address(this), loanTerms.amount, true, '0x');
     }
 
-    function calculateMonthlyPayment() internal view returns (uint256 fee, uint256 payment) {
-        uint256 grossMonthlyPayment = loanTerms.amount * ((1 + ((loanTerms.apy / 100) / 1)) ** 3);
-        uint256 calculatedTransactionFee = grossMonthlyPayment * loanTerms.transactionPercentage;
-        uint256 calculatedNetMonthlyPayment = grossMonthlyPayment - calculatedTransactionFee;
-
-        return (calculatedTransactionFee, calculatedNetMonthlyPayment);
-    }
-
-    function takeLoanAndAcceptLoanTerms() public payable onlyInState(LoanState.Funded) {
+    function acceptLoan() public payable onlyInState(LoanState.Funded) {
         borrower = payable(msg.sender);
-        dueDate = block.timestamp + (86400 * 30 * loanTerms.lockUpPeriodInMonths);
+        token.transfer(address(this), borrower, amount, true, "0x");
         loanState = LoanState.Taken;
-        // Need to check if we can send to address without LSP1 Universal Receiver
-        LSP7Mintable(tokenAddress).transfer(address(this), borrower, loanTerms.amount, true, '0x');
+    }
+
+    function calculateMonthlyPayment()
+        public
+        view
+        returns (
+            // internal
+            uint256 fee,
+            uint256 payment
+        )
+    {
+        uint256 intermediateValue = 1e18 + apy / 1e2;
+        uint256 intermediatePower = intermediateValue;
+        for (uint256 i = 1; i < 3; i++) {
+            intermediatePower = (intermediatePower * intermediateValue) / 1e18;
+        }
+        uint256 grossMonthlyPayment = (amount * intermediatePower) / 1e36 / 36;
+
+        uint256 calculatedTransactionFee = (grossMonthlyPayment *
+            (transactionPercentage)) / 1e3;
+        uint256 calculatedNetMonthlyPayment = grossMonthlyPayment -
+            calculatedTransactionFee;
+
+        return (
+            calculatedTransactionFee * 1e18,
+            calculatedNetMonthlyPayment * 1e18
+        );
     }
 
     function makePayment() public payable onlyInState(LoanState.Taken) {
         (transactionFee, netMonthlyPayment) = calculateMonthlyPayment();
 
         require(block.timestamp <= dueDate, "Payment is not due yet");
-        require(netMonthlyPayment <= remainingBalance, "There is no balance remaining on this loan");
-        require(msg.value >= netMonthlyPayment, "The message value does not cover the monthly payment");
+        require(
+            netMonthlyPayment <= amount,
+            "There is no balance remaining on this loan"
+        );
+        require(
+            msg.value >= netMonthlyPayment + transactionFee,
+            "The message value does not cover the monthly payment and transaction fee"
+        );
 
-        remainingBalance -= netMonthlyPayment;
+        amount -= netMonthlyPayment;
         dueDate += (86400 * 30);
-        LSP7Mintable(tokenAddress).transfer(borrower, lender, netMonthlyPayment, true, '0x');
-        LSP7Mintable(tokenAddress).transfer(borrower, nyxCarbonAddress, transactionFee, true, '0x');
+        token.transfer(borrower, lender, netMonthlyPayment, true, "0x");
+        token.transfer(borrower, nyxCarbonAddress, transactionFee, true, "0x");
     }
 }
---
+++ test/NonCollateralizedLoan.ts
@@ -0,0 +1,118 @@
+import { expect } from "chai";
+import { ethers } from "hardhat";
+import {
+  time,
+  loadFixture,
+} from "@nomicfoundation/hardhat-toolbox/network-helpers";
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+const { UP_ADDR, TOKEN_ADDR } = process.env;
+
+describe("Loan contract", function () {
+  async function deployLoanFixture() {
+    // Deploy token to use in loan contract
+    const Token = await ethers.getContractFactory("NyxToken");
+    const [owner, addr1, addr2] = await ethers.getSigners();
+    const hardhatToken = await Token.deploy();
+    await hardhatToken.waitForDeployment();
+
+    // Deploy loan with address of newly minted token
+    const Loan = await ethers.getContractFactory("NonCollateralizedLoan");
+    const amount: bigint = BigInt(500000);
+    const apy: bigint = BigInt(14);
+    const amortizationPeriodInMonths: bigint = BigInt(36);
+    const lockUpPeriodInMonths: bigint = BigInt(18);
+    const transactionPercentage: bigint = BigInt(8)
+
+    const hardhatLoan = await Loan.deploy(
+      amount,
+      apy,
+      amortizationPeriodInMonths,
+      lockUpPeriodInMonths,
+      transactionPercentage,
+      hardhatToken.target as string,
+      UP_ADDR as string
+    );
+    await hardhatLoan.waitForDeployment();
+
+    await hardhatToken.revokeOperator(hardhatLoan.target, "0x");
+    await hardhatToken.authorizeOperator(hardhatLoan.target, amount * BigInt(1e18), "0x");
+    
+    return { Loan, hardhatLoan, Token, hardhatToken, owner, addr1, addr2, amount, apy, lockUpPeriodInMonths, transactionPercentage };
+  }
+
+  describe("Deployment", function () {
+    it("Should set the creator as lender", async function () {
+      const { hardhatLoan, owner } = await loadFixture(deployLoanFixture);
+      expect(await hardhatLoan.lender()).to.equal(owner.address);
+    });
+
+    it("Should set the status of the loan as Created", async function () {
+      const { hardhatLoan } = await loadFixture(deployLoanFixture);
+      expect(await hardhatLoan.loanState()).to.equal(0);
+    });
+
+    it("Should assign the amount of tokens to the contract", async function () {
+      const { hardhatLoan, amount } = await loadFixture(deployLoanFixture);
+      expect(await hardhatLoan.amount() / BigInt(1e18)).to.equal(amount);
+    });
+
+    it("Should set the nyxCarbon address as nyxCarbonAddress", async function () {
+      const { hardhatLoan } = await loadFixture(deployLoanFixture);
+      expect(await hardhatLoan.nyxCarbonAddress()).to.equal(UP_ADDR);
+    });
+  });
+
+  describe("Fund Loan", function () {
+    it("Should set the status of the loan as Funded", async function () {
+      const { hardhatLoan, owner } = await loadFixture(deployLoanFixture);
+      await hardhatLoan.connect(owner).fundLoan();
+      expect(await hardhatLoan.loanState()).to.equal(1);
+    });
+
+    it("Should transfer the amount to the contract", async function () {
+      const { hardhatLoan, hardhatToken, owner, amount } = await loadFixture(deployLoanFixture);
+      await hardhatLoan.connect(owner).fundLoan();
+      expect(await hardhatToken.balanceOf(hardhatLoan.target) / BigInt(1e18)).to.equal(amount);
+    });
+  });
+
+  describe("Accept Loan", function () {
+    it("Should set addr1 as the borrower", async function () {
+      const { hardhatLoan, owner, addr1 } = await loadFixture(deployLoanFixture);
+      await hardhatLoan.connect(owner).fundLoan();
+      await hardhatLoan.connect(addr1).acceptLoan();
+      expect(await hardhatLoan.borrower()).to.equal(addr1.address);
+    });
+
+    it("Should transfer the balance to addr1", async function () {
+      const { hardhatLoan, hardhatToken, owner, addr1, amount } = await loadFixture(deployLoanFixture);
+      await hardhatLoan.connect(owner).fundLoan();
+      await hardhatLoan.connect(addr1).acceptLoan();
+      expect(await hardhatToken.balanceOf(addr1.address) / BigInt(1e18)).to.equal(amount);
+    });  
+
+    it("Should set the loan state to Taken ", async function () {
+      const { hardhatLoan, owner, addr1 } = await loadFixture(deployLoanFixture);
+      await hardhatLoan.connect(owner).fundLoan();
+      await hardhatLoan.connect(addr1).acceptLoan();
+      expect(await hardhatLoan.loanState()).to.equal(2);
+    });
+  });
+
+  describe("Calculate monthly payment & transaction fee", function () {
+    it("Should properly calculate monthly payment & transaction fee", async function () {
+      const { hardhatLoan, owner, addr1, amount, apy, transactionPercentage } = await loadFixture(deployLoanFixture);
+      await hardhatLoan.connect(owner).fundLoan();
+      await hardhatLoan.connect(addr1).acceptLoan();
+      const offChainGrossMonthlyPayment = (Number(amount) * ((1 + ((Number(apy) / 100) / 1)) ** 3)) / 36;
+      const offChainTransactionFee = offChainGrossMonthlyPayment * (Number(transactionPercentage) / 1000);
+      const offChainNetMonthlyPayment = offChainGrossMonthlyPayment - offChainTransactionFee;
+      const [ calculatedTransactionFee, calculatedNetMonthlyPayment ] = await hardhatLoan.calculateMonthlyPayment();
+
+      expect(calculatedTransactionFee / BigInt(1e18)).to.equal(BigInt(Math.round(offChainTransactionFee) - 1));
+      expect(calculatedNetMonthlyPayment / BigInt(1e18)).to.equal(BigInt(Math.round(offChainNetMonthlyPayment) + 1));
+    });
+  });
+});
