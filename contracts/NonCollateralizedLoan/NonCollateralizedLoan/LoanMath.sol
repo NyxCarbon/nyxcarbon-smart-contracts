7a822f87210a7f5b96446043ee406608b50eef82
Kyle Rozanitis <kyle.rozanitis@gmail.com>
2024-01-22 16:09:42 -0500

    NYX-108: Add library for loan math

---
+++ contracts/NonCollateralizedLoan/NonCollateralizedLoan/LoanMath.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity ^0.8.4;
+
+library LoanMath {
+    function calculateTotalLoanValue(
+        uint256 amount,
+        uint256 apy
+    ) internal view returns (uint256) {
+        uint256 intermediateValue = (((10000 + (apy / 1e16)) ** 3) / 10000);
+        return ((amount) * intermediateValue) / 1e8;
+    }
+
+    function calculateMonthlyPayment(
+        uint256 balance,
+        uint256 transactionFee,
+        uint256 months
+    ) public view returns (uint256, uint256) {
+        uint256 monthlyPayment = (balance) / (months);
+
+        uint256 fee = (monthlyPayment * transactionFee) / 10000;
+        uint256 netMonthlyPayment = monthlyPayment - fee;
+
+        return (netMonthlyPayment, fee);
+    }
+
+    function calculateProfit(
+        uint256 amount,
+        int256 units,
+        int256 price
+    ) internal view returns (int256) {
+        // calculate profit as remaining carbon credit balance * current price of carbon credits
+        // divided by the initial loan amount
+        return (units * price) - int(amount);
+    }
+}
